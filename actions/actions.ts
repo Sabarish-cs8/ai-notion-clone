a9bd89ccc6674c0d23540283ced6ff716e4d9a54
sabarish <sabarish.cs8@gmail.com>
2024-12-01 18:18:47 +0530

    GET a data from clerk in Action,types,globals.d.ts

---
+++ actions/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { adminDb } from "@/firebase-admin";
+import { auth } from "@clerk/nextjs/server";
+
+export async function createNewDocument(){
+    auth().protect();
+    
+
+
+    //Here "sessionClaim"<=in our clerk website their is one session option we set some credentials so we get that 
+
+    const {sessionClaims}=await auth();
+
+
+    //adding new document in firebase admin
+
+    const docCollectionRef = adminDb.collection("documents");
+    const docRef = await docCollectionRef.add({
+        title:"New Doc"
+    })
+
+    await adminDb.collection('users').doc(sessionClaims?.email!)
+}
