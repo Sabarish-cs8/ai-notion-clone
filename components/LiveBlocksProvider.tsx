e76e2bdbe6621d14befeaf85854aac670ce84df8
sabarish <sabarish.cs8@gmail.com>
2024-12-04 11:58:12 +0530

    Add LiveBlocks

---
+++ components/LiveBlocksProvider.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { LiveblocksProvider } from "@"
+function LiveBlocksProvider() {
+  return (
+    <div>LiveBlocksProvider</div>
+  )
+}
+
+export default LiveBlocksProvider
---
+++ lib/useOwner.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useOwner() {
+  return (
+    <div>useOwner</div>
+  )
+}
+
+export default useOwner
